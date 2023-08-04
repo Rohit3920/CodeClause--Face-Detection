bc7c42afe6a3ace9d4b3cfd063c2425c2d90de8e
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-04 21:05:18 +0530

    Camera : access the video for face Detection

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.warn("Code Clause Internship project no. 2")
 console.log("Face Detection")
+
+
+const video = document.getElementById('video');
+
+const startVideo = () =>{
+    navigator.getUserMedia(
+        {video : {}},
+        stream => video.srcObject = stream, 
+        err => console.error(err)
+    )
+}
+
+startVideo()
