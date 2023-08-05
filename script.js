f9308bea81d9982a515096dd2d28f1820b3816ba
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-05 13:24:29 +0530

    Detection : capture face exporession, landmarks and recognition

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-console.warn("Code Clause Internship project no. 2")
-console.log("Face Detection")
-
 
 const video = document.getElementById('video');
 
-const startVideo = () =>{
+const startVideo = () => {
     navigator.getUserMedia(
-        {video : {}},
-        stream => video.srcObject = stream, 
+        { video: {} },
+        stream => video.srcObject = stream,
         err => console.error(err)
     )
 }
 
-startVideo()
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
+
+
+
+video.addEventListener('play', () => {
+    console.log("face Detection");
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions();
+
+        console.log(detections)
+    }, 100)
+})
